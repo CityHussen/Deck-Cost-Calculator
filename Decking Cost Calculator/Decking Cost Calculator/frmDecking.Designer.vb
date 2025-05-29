74806ca8cb462ea02017e9a37bfb154cd94284f8
City Hussen <citym3613@gmail.com>
2025-05-29 12:32:04 -0400

    Add files via upload

---
+++ Decking Cost Calculator/Decking Cost Calculator/frmDecking.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDecking
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.lblSquareFeet = New System.Windows.Forms.Label()
+        Me.txtFootage = New System.Windows.Forms.TextBox()
+        Me.grpDeckType = New System.Windows.Forms.GroupBox()
+        Me.radComposite = New System.Windows.Forms.RadioButton()
+        Me.radRedwood = New System.Windows.Forms.RadioButton()
+        Me.radLumber = New System.Windows.Forms.RadioButton()
+        Me.lblCost = New System.Windows.Forms.Label()
+        Me.lblCostEstimate = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.picDeck = New System.Windows.Forms.PictureBox()
+        Me.grpDeckType.SuspendLayout()
+        CType(Me.picDeck, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Goudy Old Style", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(-10, 9)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(500, 55)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Decking Cost Calculator"
+        '
+        'lblSquareFeet
+        '
+        Me.lblSquareFeet.AutoSize = True
+        Me.lblSquareFeet.Font = New System.Drawing.Font("Goudy Old Style", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSquareFeet.Location = New System.Drawing.Point(15, 80)
+        Me.lblSquareFeet.Name = "lblSquareFeet"
+        Me.lblSquareFeet.Size = New System.Drawing.Size(216, 37)
+        Me.lblSquareFeet.TabIndex = 1
+        Me.lblSquareFeet.Text = "Square Footage"
+        '
+        'txtFootage
+        '
+        Me.txtFootage.Font = New System.Drawing.Font("Goudy Old Style", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFootage.ForeColor = System.Drawing.Color.Sienna
+        Me.txtFootage.Location = New System.Drawing.Point(306, 80)
+        Me.txtFootage.Name = "txtFootage"
+        Me.txtFootage.Size = New System.Drawing.Size(100, 46)
+        Me.txtFootage.TabIndex = 2
+        Me.txtFootage.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'grpDeckType
+        '
+        Me.grpDeckType.BackColor = System.Drawing.Color.AliceBlue
+        Me.grpDeckType.Controls.Add(Me.radComposite)
+        Me.grpDeckType.Controls.Add(Me.radRedwood)
+        Me.grpDeckType.Controls.Add(Me.radLumber)
+        Me.grpDeckType.Font = New System.Drawing.Font("Goudy Old Style", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.grpDeckType.ForeColor = System.Drawing.Color.Sienna
+        Me.grpDeckType.Location = New System.Drawing.Point(37, 123)
+        Me.grpDeckType.Name = "grpDeckType"
+        Me.grpDeckType.Size = New System.Drawing.Size(244, 142)
+        Me.grpDeckType.TabIndex = 3
+        Me.grpDeckType.TabStop = False
+        Me.grpDeckType.Text = "Decking Type"
+        '
+        'radComposite
+        '
+        Me.radComposite.AutoSize = True
+        Me.radComposite.Location = New System.Drawing.Point(0, 99)
+        Me.radComposite.Name = "radComposite"
+        Me.radComposite.Size = New System.Drawing.Size(160, 36)
+        Me.radComposite.TabIndex = 2
+        Me.radComposite.Text = "Composite"
+        Me.radComposite.UseVisualStyleBackColor = True
+        '
+        'radRedwood
+        '
+        Me.radRedwood.AutoSize = True
+        Me.radRedwood.Location = New System.Drawing.Point(0, 70)
+        Me.radRedwood.Name = "radRedwood"
+        Me.radRedwood.Size = New System.Drawing.Size(146, 36)
+        Me.radRedwood.TabIndex = 1
+        Me.radRedwood.Text = "Redwood"
+        Me.radRedwood.UseVisualStyleBackColor = True
+        '
+        'radLumber
+        '
+        Me.radLumber.AutoSize = True
+        Me.radLumber.Checked = True
+        Me.radLumber.Location = New System.Drawing.Point(0, 40)
+        Me.radLumber.Name = "radLumber"
+        Me.radLumber.Size = New System.Drawing.Size(318, 36)
+        Me.radLumber.TabIndex = 0
+        Me.radLumber.TabStop = True
+        Me.radLumber.Text = "Pressure-Treated Lumber"
+        Me.radLumber.UseVisualStyleBackColor = True
+        '
+        'lblCost
+        '
+        Me.lblCost.AutoSize = True
+        Me.lblCost.Font = New System.Drawing.Font("Goudy Old Style", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCost.Location = New System.Drawing.Point(15, 273)
+        Me.lblCost.Name = "lblCost"
+        Me.lblCost.Size = New System.Drawing.Size(197, 37)
+        Me.lblCost.TabIndex = 4
+        Me.lblCost.Text = "Cost Estimate"
+        '
+        'lblCostEstimate
+        '
+        Me.lblCostEstimate.AutoSize = True
+        Me.lblCostEstimate.Font = New System.Drawing.Font("Goudy Old Style", 16.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCostEstimate.Location = New System.Drawing.Point(319, 273)
+        Me.lblCostEstimate.Name = "lblCostEstimate"
+        Me.lblCostEstimate.Size = New System.Drawing.Size(138, 37)
+        Me.lblCostEstimate.TabIndex = 5
+        Me.lblCostEstimate.Text = "$0000.00"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Goudy Old Style", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(59, 322)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(138, 41)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Goudy Old Style", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(326, 322)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(116, 41)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'picDeck
+        '
+        Me.picDeck.Image = Global.Decking_Cost_Calculator.My.Resources.Resources.deck
+        Me.picDeck.Location = New System.Drawing.Point(478, -36)
+        Me.picDeck.Name = "picDeck"
+        Me.picDeck.Size = New System.Drawing.Size(220, 413)
+        Me.picDeck.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picDeck.TabIndex = 8
+        Me.picDeck.TabStop = False
+        '
+        'frmDecking
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(698, 375)
+        Me.Controls.Add(Me.picDeck)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblCostEstimate)
+        Me.Controls.Add(Me.lblCost)
+        Me.Controls.Add(Me.grpDeckType)
+        Me.Controls.Add(Me.txtFootage)
+        Me.Controls.Add(Me.lblSquareFeet)
+        Me.Controls.Add(Me.lblHeading)
+        Me.ForeColor = System.Drawing.Color.Sienna
+        Me.Name = "frmDecking"
+        Me.Text = "Decking Cost Calculator"
+        Me.grpDeckType.ResumeLayout(False)
+        Me.grpDeckType.PerformLayout()
+        CType(Me.picDeck, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents lblSquareFeet As Label
+    Friend WithEvents txtFootage As TextBox
+    Friend WithEvents grpDeckType As GroupBox
+    Friend WithEvents radComposite As RadioButton
+    Friend WithEvents radRedwood As RadioButton
+    Friend WithEvents radLumber As RadioButton
+    Friend WithEvents lblCost As Label
+    Friend WithEvents lblCostEstimate As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents picDeck As PictureBox
+End Class
---
+++ Decking Cost Calculator/Decking Cost Calculator/frmDecking.vb
@@ -0,0 +1,76 @@
+﻿'Program Name:Decking Cost Calculator Application
+'Author: City Hussen
+'Date: 07/08/2022
+'Purpose:This windows application computes the estimated cost of decking based on the
+'         number of Square feet and the following cost per square foot : Lumber =$2.35 per square foot;
+'         Redwood=$7.75 per square foot;Composite =$8.50 per Square foot.
+
+Public Class frmDecking
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        'The btnCalculate event handler calculates the estimated cost of
+        ' decking based on the square footage and the decking type.
+
+        'Declaration Section
+        Dim decFootage As Decimal
+        Dim decCostPerSquareFoot As Decimal
+        Dim decCostEstimate As Decimal
+        Dim decLumberCost As Decimal = 2.35D
+        Dim decRedwoodCost As Decimal = 7.75D
+        Dim decCompositeCost As Decimal = 8.5D
+
+        'Did user enter numeric value?
+        If IsNumeric(txtFootage.Text) Then
+            decFootage = Convert.ToDecimal(txtFootage.Text)
+            'Is Square Footage greater than zero
+            If decFootage > 0 Then
+                'Determine cost perSquare foot
+                If radLumber.Checked Then
+                    decCostPerSquareFoot = decLumberCost
+                ElseIf radRedwood.Checked Then
+                    decCostPerSquareFoot = decRedwoodCost
+                ElseIf radComposite.Checked Then
+                    decCostPerSquareFoot = decCompositeCost
+                End If
+                'Calculate and display the cost estimate 
+                decCostEstimate = decFootage * decCostPerSquareFoot
+                lblCostEstimate.Text = decCostEstimate.ToString("C")
+            Else
+                'Display error message if user entered a negative value
+                MsgBox("You entered " & decFootage.ToString() & "Enter a Positive Number", , "Input Error")
+                txtFootage.Text = ""
+                txtFootage.Focus()
+            End If
+        Else
+            'Display error message if user entered a nonnumeric value
+            MsgBox("Enter the Square Footage of Decking", , "Input Error")
+            txtFootage.Text = ""
+            txtFootage.Focus()
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'This event handler is executed when the user clicks the clear button.It 
+        'clears the square footage text box and the cost estimate label,resets the radio
+        'button with the Lumber selected and setsthe focus to the square footage text box.
+
+        txtFootage.Clear()
+        lblCostEstimate.Text = ""
+        radLumber.Checked = True
+        radRedwood.Checked = False
+        radComposite.Checked = False
+        txtFootage.Focus()
+    End Sub
+
+    Private Sub frmDecking_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'This event handler is executed when the form is located at the start of
+        'the program.It sets the focus to the square footage text box and 
+        'clears the cost estimate label.
+
+        txtFootage.Focus()
+        lblCostEstimate.Text = ""
+    End Sub
+
+    Private Sub radLumber_CheckedChanged(sender As Object, e As EventArgs) Handles radLumber.CheckedChanged
+
+    End Sub
+End Class
